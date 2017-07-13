d8d3c9ecbb47def174d306e78c1504742bb93621
CarolineClark <carolinejaneclark@gmail.com>
2017-07-13 11:00:59 +0100

    Added VaseModel prefab to show broken vase after interaction

--- Assets/Vase.cs
+++ Assets/Vase.cs
@@ -7,10 +7,16 @@ public class Vase : MonoBehaviour {
 	private AudioClip vaseSound;
 	private AudioSource audioSource;
 	private bool smashed = false;
+	private GameObject vaseUnbroken;
+	private GameObject vaseBroken;
 
 	void Start () {
 		vaseSound = Resources.Load<AudioClip>("FinalSounds/Vase Break");
 		audioSource = GetComponent<AudioSource>();
+		vaseUnbroken = transform.Find("VaseUnbroken").gameObject;
+		vaseUnbroken.SetActive(true);
+		vaseBroken = transform.Find("VaseBroken").gameObject;
+		vaseBroken.SetActive(false);
 	}
 
 	void OnTriggerEnter(Collider other) {
@@ -18,6 +24,8 @@ public class Vase : MonoBehaviour {
 			smashed = true;
 			audioSource.Play();
 			EventManager.TriggerEvent(Constants.BREAK_POT_EVENT);
+			vaseBroken.SetActive(true);
+			vaseUnbroken.SetActive(false);
 		}
 	}
 }
